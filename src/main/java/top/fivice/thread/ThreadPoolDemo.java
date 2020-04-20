8199d7c6f15fba5047fdeba5c527b8d1d9e84f72
Fivice <wubin_oct@163.com>
2020-04-20 23:13:03 +0800

    线程池

--- src/main/java/top/fivice/thread/ThreadPoolDemo.java
+++ src/main/java/top/fivice/thread/ThreadPoolDemo.java
@@ -1,7 +1,29 @@
 package top.fivice.thread;
 
+import java.util.concurrent.*;
+
 public class ThreadPoolDemo {
     public static void main(String[] args) {
-        
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+        scheduledExecutorService.scheduleWithFixedDelay(new Task("Task-1"),1,3, TimeUnit.SECONDS);
+
+    }
+}
+class Task implements Runnable {
+    private final String name;
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("start task " + name);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("end task " + name);
     }
 }
