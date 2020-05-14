9df2e88d07282068df1edfb0c73624a0136c1d86
Fivice <wubin_oct@163.com>
2020-05-14 22:20:48 +0800

    idea配置文件

--- src/main/java/top/fivice/concurrent/chapters_3/ThisEscape.java
+++ src/main/java/top/fivice/concurrent/chapters_3/ThisEscape.java
@@ -1,6 +1,28 @@
 package top.fivice.concurrent.chapters_3;
 
 public class ThisEscape {
-    public ThisEscape() {
+    private int count;
+    public ThisEscape(EventSource source) {
+        source.registerListener(new EventListener() {
+            public void onEvent(Event e) {
+                doSomething(e);
+                
+            }
+        });
+    }
+
+    void doSomething(Event e) {
+    }
+
+
+    interface EventSource {
+        void registerListener(EventListener e);
+    }
+
+    interface EventListener {
+        void onEvent(Event e);
+    }
+
+    interface Event {
     }
 }
